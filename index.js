a92d6baedbf2432cfaa6ae007122d8e7d99dc037
Lucaskamminga <kammingalucas@gmail.com>
2021-10-08 15:09:33 +0200

    first commit

--- index.js
+++ index.js
@@ -4,8 +4,5 @@ const PORT = process.env.PORT || 3000;
  
 app.use(express.static('public'));
  
-app.get('/', (req, res) => {
-    res.send('Hoi!');
-});
- 
+
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
